51db669f097174e70994934fdfcb02044cd2f60b
hasanmtasnimul <60707922+hasanmtasnimul@users.noreply.github.com>
2022-11-29 07:51:37 +0600

    removed console logs

--- src/CurrentQuestion.jsx
+++ src/CurrentQuestion.jsx
@@ -11,9 +11,25 @@ export default function CurrentQuestion(props) {
         }
         return array;
     }
+
+    function checkAnswer(selectedValue) {
+        if (selectedValue === correctAnswer) {
+            props.updateScore();
+
+        }
+        const newOptions = optionsArray.map((item) => {
+            return (
+                <div className='quiz-option' key={item}>
+                    {((item === correctAnswer)) && <FontAwesomeIcon className='fa-check' icon={faCheck} />}
+                    {((item === selectedValue)) && (selectedValue !== correctAnswer) && <FontAwesomeIcon className='fa-xmark' icon={faXmark} />}
+                    {item}
+                </div >
+            )
+        });
+        setOptions(newOptions);
+    }
     const question = props.item.question;
     const questionNumber = props.questionNumber;
-
     const correctAnswer = props.item.correctAnswer;
     const optionsArray = shuffleOptions(props.item.incorrectAnswers.concat([correctAnswer]));
     const [options, setOptions] = useState();
@@ -28,102 +44,6 @@ export default function CurrentQuestion(props) {
         }))
     }, [question])
 
-
-    function checkAnswer(selectedValue) {
-        console.log(selectedValue);
-        console.log(correctAnswer);
-
-        // setSelectedOption(value);
-        const newOptions = optionsArray.map((item) => {
-            return (
-                <div className='quiz-option' key={item}>
-                    {((item === correctAnswer)) && <FontAwesomeIcon className='fa-check' icon={faCheck} />}
-                    {((item === selectedValue)) && (selectedValue !== correctAnswer) && <FontAwesomeIcon className='fa-xmark' icon={faXmark} />}
-                    {item}
-                </div >
-            )
-        });
-        setOptions(newOptions);
-        if (selectedValue === correctAnswer) {
-            console.log("correct");
-            // props.setScore((prev) => { return prev + 1 })
-            props.updateScore();
-        }
-        else {
-            console.log("incorrect");
-
-        }
-    }
-
-    // const setCurrentProp = async () => {
-    //     await setQuestionNumber(props.questionNumber);
-    //     await setQuestion(props.item.question);
-    //     await setCorrectAnswer(props.item.correctAnswer);
-    //     console.log("this line ran");
-    //     console.log(correctAnswer);
-    //     await setOptionsArray(shuffleOptions(props.item.incorrectAnswers.concat([props.item.correctAnswer])));
-    //     const newOptions = await optionsArray.map((item) => {
-    //         return (
-    //             <div className='quiz-option' key={item} onClick={() => { checkAnswer(item) }}>
-    //                 {item}
-    //             </div >
-    //         )
-    //     });
-    //     await setOptions(newOptions);
-    //     console.log(question);
-    //     console.log(correctAnswer);
-    //     console.log(optionsArray);
-    //     console.log(props.item.question)
-    // }
-
-    // useEffect(() => {
-    //     setCurrentProp();
-    // }, [questionNumber])
-
-    // useEffect(() => {
-    //     const newOptions = optionsArray.map((item) => {
-    //         return (
-    //             <div className='quiz-option' key={item} onClick={() => { checkAnswer(item) }}>
-    //                 {item}
-    //             </div >
-    //         )
-    //     });
-    //     setOptions(newOptions);
-    // }, [props])
-
-
-
-    // useEffect(() => {
-    //     const newOptions = optionsArray.map((item) => {
-    //         return (
-    //             <div className='quiz-option' key={item} onClick={() => { checkAnswer(item) }}>
-    //                 {item}
-    //             </div >
-    //         )
-    //     });
-    //     console.log('options changed')
-    //     setOptions(newOptions);
-    //     console.log(options);
-    // }, [props])
-
-    // useEffect(() => {
-    //     const newOptions = optionsArray.map((item) => {
-    //         return (
-    //             <div className='quiz-option' key={item} onClick={() => { !attempted && checkAnswer(item) }}>
-    //                 {(attempted && (item === correctAnswer)) && <FontAwesomeIcon className='fa-check' icon={faCheck} />}
-    //                 {(attempted && (item === selectedOption)) && (selectedOption !== correctAnswer) && <FontAwesomeIcon className='fa-xmark' icon={faXmark} />}
-    //                 {item}
-    //             </div >
-    //         )
-    //     });
-    //     console.log('tick marking part ran')
-    //     setOptions(newOptions);
-    // }, [attempted])
-
-    useEffect(() => {
-        console.log(question);
-        console.log(correctAnswer);
-    }, [props])
     return (
         <div className='current-quiz-question'>
             <div className='quiz-question'>
